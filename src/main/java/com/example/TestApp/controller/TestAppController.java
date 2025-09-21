724f1b095f79dc7427da5eac4f559de3f0ecb66e
Srinath318 <187srinath@gmail.com>
2025-09-21 21:22:57 +0530

    frontend CORS

--- src/main/java/com/example/TestApp/controller/TestAppController.java
+++ src/main/java/com/example/TestApp/controller/TestAppController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "https://test-crud-app.netlify.app/")
 public class TestAppController {
 
     @Autowired
